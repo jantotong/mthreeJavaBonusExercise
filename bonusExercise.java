24dad712080082f0ba44fe62075e8728dea3c982
jantotong <43649573+jantotong@users.noreply.github.com>
2021-08-30 13:57:43 -0700

    Update bonusExercise.java
    Added try catch

--- bonusExercise.java
+++ bonusExercise.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;  // Import the Scanner class
 
 public class bonusExercise {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        int total = 0;
-        for (int i = 1; i<=n; i++){
-            if(i % 3 == 0 || i % 5 == 0){
-                total += i;
+        System.out.println("Enter a number and display its multiples of 3 and 5: ");
+        try {
+            int n = Integer.parseInt(sc.nextLine());
+            int total = 0;
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    total += i;
+                }
             }
+            System.out.println(total);
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter an integer type, not anything else!");
         }
-        System.out.println(total);
     }
 }
