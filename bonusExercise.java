88bda1902ac2de1c4cc7ffb44d85fa47d4956a83
jantotong <43649573+jantotong@users.noreply.github.com>
2021-08-30 13:43:58 -0700

    Update bonusExercise.java

--- bonusExercise.java
+++ bonusExercise.java
@@ -4,10 +4,12 @@ public class bonusExercise {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
+        int total = 0;
         for (int i = 1; i<=n; i++){
             if(i % 3 == 0 || i % 5 == 0){
-                System.out.println(i);
+                total += i;
             }
         }
+        System.out.println(total);
     }
 }
